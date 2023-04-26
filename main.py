a8f05764baf30f4831c2a33335541cb0ecd2d95a
Jesse Riggins <jriggins@users.noreply.github.com>
2023-04-25 19:31:46 -0500

    Add Antenna Pot and UI updates.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 try:
-  from machine import Pin
+  from machine import Pin, ADC
 except:
   class Pin:
     OUT = "OUT"
@@ -20,6 +20,20 @@ except:
       return str({
         "id": self._id,
         "value": self.value
+      })
+
+  class ADC:
+    def __init__(self, pin):
+      self._pin = pin
+
+    def read_u16(self):
+      import random
+      return random.randint(0, 65535)
+
+    def __str__(self):
+      return str({
+        "id": str(self._pin),
+        "read_u16": self.read_u16()
       })
 
 try:
@@ -54,6 +68,20 @@ try:
   import usocket as socket
 except:
   import socket
+
+
+class AntennaPot:
+  def __init__(self, adc):
+    self._adc = adc
+    self._name = "Antenna Pot"
+
+  def current_voltage(self):
+    pot_val = self._adc.read_u16()
+    voltage = (3.3/65106) * pot_val - (430*3.3/65106)
+    print(voltage)
+    print(pot_val)
+
+    return voltage
 
 class Brake:
   def __init__(self, relay):
@@ -107,6 +135,7 @@ class ControlBox:
     self._ccw_rotor = Rotor(relay=Pin(3,Pin.OUT,1), name="CCW")
     self._cw_rotor = Rotor(relay=Pin(4,Pin.OUT,1), name="CW")
     self._brake_pause_time_in_seconds = brake_pause_time_in_seconds
+    self._antenna_pot = AntennaPot(adc=ADC(Pin(28)))
 
   def is_ccw_rotor_enabled(self):
     return self._ccw_rotor.is_enabled()
@@ -139,12 +168,16 @@ class ControlBox:
     self._sleep(self._brake_pause_time_in_seconds)
     self._brake.enable()
 
+  def current_antenna_pot_voltage(self):
+    return self._antenna_pot.current_voltage()
+
   def __str__(self):
     return str({
       "brake": str(self._brake),
       "ccw_rotor": str(self._ccw_rotor),
       "cw_rotor": str(self._cw_rotor),
-      "brake_pause_time_in_seconds": self._brake_pause_time_in_seconds
+      "brake_pause_time_in_seconds": self._brake_pause_time_in_seconds,
+      "antenna_pot": str(self._antenna_pot),
     })
 
 
@@ -155,6 +188,7 @@ def web_server():
   html = """<html>
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/css-spinning-spinners/1.1.0/load5.css"/>
       <style>
         body{font-family:Arial; text-align: center; margin: 0px auto; padding-top:30px;}
         .switch{position:relative;display:block;width:120px;height:68px;margin-bottom:10px}.switch input{display:none}
@@ -162,24 +196,29 @@ def web_server():
         .slider:before{position:absolute;content:"";height:52px;width:52px;left:8px;bottom:8px;background-color:#fff;-webkit-transition:.4s;transition:.4s;border-radius:68px}
         input:checked+.slider{background-color:#2196F3}
         input:checked+.slider:before{-webkit-transform:translateX(52px);-ms-transform:translateX(52px);transform:translateX(52px)}
-        #control_box{margin:auto; width:10%;}
-        #control_box .label{position:relative; right:100%; top:40%}
+        #control_box{margin:auto; width:10%%;}
+        #control_box .label{position:relative; right:100%%; top:40%%}
+        #control_box .output {position: relative; bottom: 0; color: #33d533; background: black; padding: 10 10 10 100}
+        #antenna_pot .label { bottom: 23px; right: 146px; top: 22px }
+        .loading{background:gray; pointer-events:none;}
       </style>
       <script>
-        function toggleCheckbox(element) {
-          var xhr = new XMLHttpRequest();
-          if(element.checked) {
-            xhr.open("GET", "/?relay=on", true);
-          }
-          else {
-            xhr.open("GET", "/?relay=off", true);
-          }
-          xhr.send();
+        function sleep (time) {
+          return new Promise((resolve) => setTimeout(resolve, time));
         }
 
         function controlAntennaRotation(element) {
           var xhr = new XMLHttpRequest();
           var command = element.value;
+
+          xhr.addEventListener("load", () => {
+            location.reload();
+          });
+
+          requestAnimationFrame(() => {
+            document.querySelector("body").classList.add("loading");
+          });
+
           console.log("rotation %%o", command);
           xhr.open("POST", "/" + command, true);
           xhr.send();
@@ -191,24 +230,34 @@ def web_server():
       <form id="control_box" action="#">
         <label class="switch">
           <span class="label">Off</span>
-          <input type="radio" id="stop_antenna_rotation" name="direction" value="stop_antenna_rotation" onchange="controlAntennaRotation(this)"><span class="slider"></span>
+          <input type="radio" id="stop_antenna_rotation" name="direction" value="stop_antenna_rotation" onchange="document.body.classList.add('loading');document.body.classList.add('loading');controlAntennaRotation(this)"><span class="slider" %(is_all_off)s></span>
         </label>
         <label class="switch">
           <span class="label">Clockwise</span>
-          <input type="radio" id="rotate_antenna_clockwise" name="direction" value="rotate_antenna_clockwise" onchange="controlAntennaRotation(this)"><span class="slider"></span>
+          <input type="radio" id="rotate_antenna_clockwise" name="direction" value="rotate_antenna_clockwise" onchange="controlAntennaRotation(this)" %(is_cw_rotor_enabled)s><span class="slider"></span>
         </label>
         <label class="switch">
           <span class="label">Counter Clockwise</span>
-          <input type="radio" id="rotate_antenna_counter_clockwise" name="direction" value="rotate_antenna_counter_clockwise" onchange="controlAntennaRotation(this)"><span class="slider"></span>
+          <input type="radio" id="rotate_antenna_counter_clockwise" name="direction" value="rotate_antenna_counter_clockwise" onchange="controlAntennaRotation(this)" %(is_ccw_rotor_enabled)s><span class="slider"></span>
+        </label>
+        <label id="antenna_pot">
+          <span class="label">Antenna Pot</span>
+          <span class="output">%(current_antenna_pot_voltage)0.5f</span>
         </label>
       </form>
     </body>
-  </html>"""
+  </html>""" % {
+    "current_antenna_pot_voltage": control_box.current_antenna_pot_voltage(),
+    "is_ccw_rotor_enabled": "checked" if control_box.is_ccw_rotor_enabled() else "",
+    "is_cw_rotor_enabled": "checked" if control_box.is_cw_rotor_enabled() else "",
+    "is_all_off": "checked" if (not control_box.is_cw_rotor_enabled() and not control_box.is_ccw_rotor_enabled()) else "",
+  }
   return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
+
 
 while True:
   try:
@@ -227,6 +276,8 @@ while True:
       getattr(control_box, command)()
 
     response = web_server()
+    print(f"response: {response}")
+
     conn.send(b'HTTP/1.1 200 OK\n')
     conn.send(b'Content-Type: text/html\n')
     conn.send(b'Connection: close\n\n')
